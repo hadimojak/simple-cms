e9b2bf43794e7850ca755e2cdfaa8dd83c1ba660
Hadi Arbabi <h.arbabi@cobex.ir>
2021-10-19 17:04:15 +0330

    pdf thumbnail working

--- controller/editor.js
+++ controller/editor.js
@@ -1,9 +1,8 @@
-const { JSDOM } = require("jsdom");
-const { window } = new JSDOM("");
-const $ = require("jquery")(window);
 const fs = require('fs');
 const path = require('path');
-
+const pdf = require('pdf-thumbnail');
+const gm = require('gm');
+const { PDFNet } = require('@pdftron/pdfnet-node');
 
 exports.getEditorProfile = (req, res, next) => {
     res.render('editor/editorHome', { pageTitle: 'editor', path: '/editor' });
@@ -17,8 +16,8 @@ exports.postNewPost = async (req, res, next) => {
     // const editorId = req.params.editorId;
     // console.log(req.body.post);
     const newPost = req.body.post;
-    const postFielName = Date.now() + 'post' + ".html";
-    fs.writeFileSync(path.join(__dirname, '..', 'uploads', 'posts', postFielName)
+    const postFileName = Date.now() + 'post' + ".html";
+    fs.writeFileSync(path.join(__dirname, '..', 'uploads', 'posts', postFileName)
         , newPost, (err) => { console.log(err); });
 
 
@@ -41,15 +40,31 @@ exports.postEditPost = (req, res, next) => {
 };
 
 exports.getAllFiles = (req, res, next) => {
-    const imagesArray = [];
-    const images = fs.readdirSync(path.join(__dirname, '..', 'uploads', 'media'),
-        { encoding: 'utf8' });
-    images.forEach(p => {
-        imagesArray.push('/uploads/media/' + p);
+    const fileArray = [];
+    const definitelyPosix = path.join(__dirname, '..', 'uploads', 'media').split(path.sep).join(path.posix.sep);
+    fs.readdir(definitelyPosix, (err, files) => {
+        if (err)
+            console.log(err);
+        else {
+            files.forEach(async file => {
+                if (file.split('.')[1] === 'pdf') {
+
+
+                    // const definitelyPosix = path.join(__dirname, '..', 'uploads', 'media', file).split(path.sep).join(path.posix.sep);
+                    // const pdfBuffer = fs.readFileSync(definitelyPosix);
+                    // console.log(pdfBuffer);
+                    // pdf(pdfBuffer, options).then(data => {
+                    //     fs.writeFileSync(path.join(__dirname, '..', 'uploads', 'media', 'thumb_' + file)
+                    //         , data, (err) => { console.log(err); });
+
+                    // }).catch(err => { console.log(err); });
+                }
+
+            });
+        }
+        res.render('editor/allFiles', { pageTitle: 'فایل ها', path: '/storage' });
+
     });
-
-    res.render('editor/allFiles', { pageTitle: 'فایل ها', path: '/storage', imageUrl: imagesArray, imageName: images });
-
 };
 
 exports.filePreview = (req, res, next) => {
@@ -64,6 +79,19 @@ exports.deleteFile = (req, res, next) => {
 
 
 exports.postUpload = (req, res, next) => {
-    console.log(req.body, req.file);
-    res.redirect('/editor/storage')
+    const fileName = req.file.originalname;
+    if (fileName.split('.')[1] === 'pdf') {
+        PDFNet.initialize('demo:1634646868519:78bd88e90300000000a42b22ff1a99ac02686e42e084032143298c2e2f')
+            .then(async data => {
+                const posixFilePath = path.join(__dirname, '..', 'uploads', 'media', fileName).split(path.sep).join(path.posix.sep);
+                const doc = await PDFNet.PDFDoc.createFromFilePath(posixFilePath);
+                const thumb = await PDFNet.PDFDraw.create(90);
+                const pg = await doc.getPage(1);
+                await thumb.export(pg, path.join(__dirname, '..', 'uploads', 'thumb', `thumb_${fileName.split('.')[0]}.png`).split(path.sep).join(path.posix.sep), 'png');
+                PDFNet.shutdown();
+            });
+    } else if (fileName.split('.')[1] === 'jpg')
+
+
+        res.redirect('/editor/storage');
 };
