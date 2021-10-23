4ef4c188a6adcdc7beaf54f3d0ff51185481a34e
Hadi Arbabi <h.arbabi@cobex.ir>
2021-10-23 09:18:45 +0330

    models

--- models/model.js
+++ models/model.js
@@ -32,7 +32,7 @@ const User = sequelize.define('User', {
     updatedAt: Sequelize.DATE
 }, {
     sequelize: sequelize, freezeTableName: true,
-    modelName: "user", paranoid: true
+    modelName: "user"
 });
 
 const Media = sequelize.define('Media', {
@@ -57,10 +57,83 @@ const Media = sequelize.define('Media', {
     updatedAt: Sequelize.DATE
 }, {
     sequelize: sequelize, freezeTableName: true,
-    modelName: "user", paranoid: true
+    modelName: "user"
+});
+
+const Post = sequelize.define('Post', {
+    postName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }, path: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE
+}, {
+    sequelize: sequelize, freezeTableName: true,
+    modelName: "post"
+});
+
+const Menu = sequelize.define('Menu', {
+    menuId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    }, menuName: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    }, path: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE
+}, {
+    sequelize: sequelize, freezeTableName: true,
+    modelName: "menu"
+});
+
+const Setting = sequelize.define('Setting', {
+    settingId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    }, path: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE
+}, {
+    sequelize: sequelize, freezeTableName: true,
+    modelName: "setting"
+});
+
+const Page = sequelize.define('Page', {
+    pageId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    }, pageName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }, action: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }, path: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE
+}, {
+    sequelize: sequelize, freezeTableName: true,
+    modelName: "page"
 });
 
 User.hasMany(Media, { onDelete: 'NO ACTION', onUpdate: 'NO ACTION' });
 Media.belongsTo(User);
+User.hasMany(Post, { onDelete: 'NO ACTION', onUpdate: 'NO ACTION' });
+Post.belongsTo(User);
+User.hasMany(Menu, { onDelete: 'NO ACTION', onUpdate: 'NO ACTION' });
+Menu.belongsTo(User);
 
-module.exports = { User };
+module.exports = { User,Media,Menu,Page,Setting,Post };
