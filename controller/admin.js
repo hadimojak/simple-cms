6f2d43b0eec6295925425eff67e8445d5161868d
Hadi Arbabi <h.arbabi@cobex.ir>
2021-10-25 11:39:14 +0330

    media deletion

--- controller/admin.js
+++ controller/admin.js
@@ -177,38 +177,26 @@ exports.deleteUser = (req, res, next) => {
     const phoneNumber = req.params.userPhoneNumber;
     User.destroy({ where: { phoneNumber: phoneNumber } })
         .then(user => {
-            res.json({ data: 'user deleted succesfully' });
-        });
+            res.json({ data: user + ' user deleted succesfully' });
+        }).catch(err => { console.log(err); });
 };
 
 // admin files
 exports.filesApi = (req, res, next) => {
     const fileArray = [];
-    const definitelyPosix = path.join(__dirname, '..', 'uploads', 'media').split(path.sep).join(path.posix.sep);
-    fs.readdir(definitelyPosix, (err, files) => {
-        if (err)
-            console.log(err);
-        else {
-            files.forEach(file => {
-                if (file.split('.')[1] === 'png' || file.split('.')[1] === 'jpg' || file.split('.')[1] === 'jpeg') {
-                    fileArray.push({ src: `/uploads/media/${file}`, thumb: `/uploads/thumb/${file}`, fileName: file, ext: file.split('.')[1] });
-                } else if (file.split('.')[1] === 'pdf') {
-                    fileArray.push({ src: `/uploads/media/${file}`, thumb: `/pictures/pdf.png`, fileName: file, ext: file.split('.')[1] });
-                } else if (file.split('.')[1] === 'rar' || file.split('.')[1] === 'zip') {
-                    fileArray.push({ src: `/uploads/media/${file}`, thumb: `/pictures/file.png`, fileName: file, ext: file.split('.')[1] });
-                } else if (file.split('.')[1] === 'mp4' || file.split('.')[1] === 'mkv') {
-                    fileArray.push({ src: `/uploads/media/${file}`, thumb: `/pictures/video.png`, fileName: file, ext: file.split('.')[1] });
-                }
-            });
-            res.json(fileArray);
-        }
-    });
+    Media.findAll().then(files => {
+        return files.forEach(file => {
+            fileArray.push(file.dataValues);
+
+        });
+    }).then(data => {
+        res.json(fileArray);
+    }).catch(err => { console.log(err); });
 };
 exports.getAllFiles = (req, res, next) => {
     res.render('admin/allFiles', { pageTitle: 'فایل ها', path: '/storage' });
 };
 exports.postUploadFile = (req, res, next) => {
-    console.log(req.file);
     const ext = path.extname(req.file.originalname);
     const options = { width: 128, height: 128 };
     let thumbLoc;
@@ -220,16 +208,35 @@ exports.postUploadFile = (req, res, next) => {
             .catch(err => { console.log(err); });
         thumbLoc = `/uploads/thumb/${req.file.filename}`;
     }
-    thumbLoc = '/pictures/pdf.png';
+    if (ext === '.pdf') {
+        thumbLoc = '/pictures/pdf.png';
+    }
+    if (ext === '.rar' || ext === '.zip') {
+        thumbLoc = '/pictures/file.png';
+    } if (ext === '.mp4' || ext === '.mkv' || ext === '.3gp') {
+        thumbLoc = '/pictures/video.png';
+    }
     Media.create({
-        fileName: req.file.filename, originalName: req.file.originalname, thumb: thumbLoc,
-        path: (req.file.destination).split('.')[1], mimetype: req.file.mimetype, size: req.file.size / 1000
+        fileName: req.file.filename,
+        originalName: req.file.originalname,
+        thumb: thumbLoc,
+        path: (req.file.destination).split('.')[1],
+        mimetype: req.file.mimetype,
+        ext: ext,
+        size: req.file.size / 1000,
+        UserId: 2,
     }).then(file => {
         res.redirect('/admin/storage');
     });
 };
 exports.deleteFile = (req, res, next) => {
-    res.json({ data: 'deleteFile' });
+    const fileName = req.params.fileName;
+    Media.destroy({ where: { fileName: fileName } })
+        .then(file => {
+            return res.json({ data: file + " is deleted" });
+        })
+        .then(data => console.log(data))
+        .catch(err => { console.log(err); });
 };
 
 // admin menus
--- sequelize.js
+++ sequelize.js
@@ -2,8 +2,9 @@ const { Sequelize, DataTypes, Model } = require('sequelize');
 const connection = {
     host: 'localhost', dialect: 'mysql', dialectOptions: {
         supportBigNumbers: true,
-        bigNumberStrings: true
-    }
+        bigNumberStrings: true, useUTC: false, dateStrings: true,
+        typeCast: true
+    },timezone: '+03:30'
 };
 
 const sequelize = new Sequelize('cms', 'root', '2525', connection
